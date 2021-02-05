08fcad40b93809e4d526b53c3b747222be82e5b2
Vafin Artem <a.vafin85@gmail.com>
2021-02-05 12:39:34 +0500

    добавил компоненты

---
+++ src/components/not-found-page/not-found-page.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <React.Fragment>
+      <div className="user-page">
+        <header className="page-header user-page__head">
+          <div className="logo">
+            <a href="main.html" className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+          <h1 className="page-title user-page__title">404 Not Found</h1>
+          <div className="user-block">
+            <div className="user-block__avatar">
+              <img src="img/avatar.jpg" alt="User avatar" width={63} height={63} />
+            </div>
+          </div>
+        </header>
+        <section className="catalog">
+          <link to="/" />Вернуться на главную
+        </section>
+        <footer className="page-footer">
+          <div className="logo">
+            <a href="main.html" className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
+      </div>
+
+    </React.Fragment>
+  );
+};
+
+export default NotFoundPage;
