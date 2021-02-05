7c4e7341375a738af1759cba6913612c4684605d
Vafin Artem <a.vafin85@gmail.com>
2021-02-05 16:55:41 +0500

    добавил маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import Main from '../main/main';
+import Movie from '../movie/movie';
+import MyList from '../my-list/my-list';
+import AddReview from '../add-review/add-review';
+import SignIn from '../sign-in/sign-in';
+import Player from '../player/player';
+import NotFoundPage from '../not-found-page/not-found-page';
 
 const App = ({moviesCount, title, genre, releaseYear}) => {
-
   return (
-    <Main
-      moviesCount={moviesCount}
-      title={title}
-      genre={genre}
-      releaseYear={releaseYear}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main
+            moviesCount={moviesCount}
+            title={title}
+            genre={genre}
+            releaseYear={releaseYear}
+          />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/films/:id">
+          <Movie />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReview />
+        </Route>
+        <Route exact path="/player/:id">
+          <Player />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
