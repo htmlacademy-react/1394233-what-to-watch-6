982d214ee09f716c29a86a1528c0397dfe7d62a8
Vafin Artem <a.vafin85@gmail.com>
2021-02-11 17:22:38 +0500

    добавил вывод описания рейтинга

--- src/components/movie/movie.jsx
+++ src/components/movie/movie.jsx
@@ -2,6 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Link, useHistory} from 'react-router-dom';
 
+const FilmRatings = {
+  Bad: {
+    DESCRIPTION: `Bad`,
+    RATING: 0
+  },
+  Normal: {
+    DESCRIPTION: `Normal`,
+    RATING: 3
+  },
+  Good: {
+    DESCRIPTION: `Good`,
+    RATING: 5
+  },
+  VeryGood: {
+    DESCRIPTION: `Very Good`,
+    RATING: 8
+  },
+  Awesome: {
+    DESCRIPTION: `Awesome`,
+    RATING: 10
+  }
+};
+
+const getDescriptionRating = (rating) => {
+  if (rating < FilmRatings.Normal.RATING) {
+    return FilmRatings.Bad.DESCRIPTION;
+  } else if (rating >= FilmRatings.Normal.RATING && rating < FilmRatings.Good.RATING) {
+    return FilmRatings.Normal.DESCRIPTION;
+  } else if (rating >= FilmRatings.Good.RATING && rating < FilmRatings.VeryGood.RATING) {
+    return FilmRatings.Good.DESCRIPTION;
+  } else if (rating >= FilmRatings.VeryGood.RATING && rating < FilmRatings.Awesome.RATING) {
+    return FilmRatings.VeryGood.DESCRIPTION;
+  }
+
+  return FilmRatings.Awesome.RATING;
+};
+
 const Movie = ({film}) => {
   const {
     backgroundImage,
@@ -88,7 +125,7 @@ const Movie = ({film}) => {
               <div className="movie-rating">
                 <div className="movie-rating__score">{rating}</div>
                 <p className="movie-rating__meta">
-                  <span className="movie-rating__level">Very good</span>
+                  <span className="movie-rating__level">{getDescriptionRating(rating)}</span>
                   <span className="movie-rating__count">{scoresCount} ratings</span>
                 </p>
               </div>
