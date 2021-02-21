e067c35971ec1dedea77d5d807a823dd1a716b3a
Vafin Artem <a.vafin85@gmail.com>
2021-02-22 01:26:30 +0500

    реализовал переключение табов

--- src/components/movie-tabs/movie-tabs.jsx
+++ src/components/movie-tabs/movie-tabs.jsx
@@ -1,30 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-// import MovieDetails from '../movie-details/movie-details';
-// import MovieOverview from '../movie-overview/movie-overview';
+import MovieDetails from '../movie-details/movie-details';
+import MovieOverview from '../movie-overview/movie-overview';
 import MovieReview from '../movie-reviews/movie-reviews';
 import {MOVIES_PROP, REVIEW_PROP} from '../../utils/validate';
 
-const MovieTabs = ({reviews}) => {
+const MovieTabs = ({film, reviews}) => {
 
+  const getActiveTab = () => {
+    if (activeTab.details) {
+      return <MovieDetails film={film} />;
+    } else if (activeTab.overview) {
+      return <MovieOverview film={film} />;
+    }
+    return <MovieReview reviews={reviews} />;
+  };
+
+  const [activeTab, setActiveTab] = useState({overview: true, details: false, review: false});
   return (
     <React.Fragment>
       <nav className="movie-nav movie-card__nav">
         <ul className="movie-nav__list">
-          <li className="movie-nav__item movie-nav__item--active">
-            <a href="#" className="movie-nav__link">Overview</a>
+          <li className={activeTab.overview ? `movie-nav__item movie-nav__item--active` : `movie-nav__item`}>
+            <a href="#" className="movie-nav__link" onClick={
+              (evt) => {
+                evt.preventDefault();
+                setActiveTab({
+                  overview: true
+                });
+              }
+            }>Overview</a>
           </li>
-          <li className="movie-nav__item">
-            <a href="#" className="movie-nav__link">Details</a>
+          <li className={activeTab.details ? `movie-nav__item movie-nav__item--active` : `movie-nav__item`}>
+            <a href="#" className="movie-nav__link" onClick={
+              (evt) => {
+                evt.preventDefault();
+                setActiveTab({
+                  details: true
+                });
+              }
+            }>Details</a>
           </li>
-          <li className="movie-nav__item">
-            <a href="#" className="movie-nav__link">Reviews</a>
+          <li className={activeTab.review ? `movie-nav__item movie-nav__item--active` : `movie-nav__item`}>
+            <a href="#" className="movie-nav__link" onClick={
+              (evt) => {
+                evt.preventDefault();
+                setActiveTab({
+                  review: true
+                });
+              }
+            }>Reviews</a>
           </li>
         </ul>
       </nav>
-      <MovieReview
-        reviews={reviews}
-      />
+      {getActiveTab()}
     </React.Fragment>
   );
 };
