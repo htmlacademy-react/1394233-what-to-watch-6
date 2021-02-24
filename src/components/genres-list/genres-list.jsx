d1e12258c58468ecc1b5626108a1dce1f1c044dc
Vafin Artem <a.vafin85@gmail.com>
2021-02-24 19:43:56 +0500

    рефакторинг

--- src/components/genres-list/genres-list.jsx
+++ src/components/genres-list/genres-list.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {ActionCreator} from '../../store/action';
-import {FiltersType} from '../../consts';
+import {FiltersTabName} from '../../consts';
 import GenreTab from '../genre-tab/genre-tab';
 
 const GenresList = ({onChangeGenres}) => {
   return (
     <ul className="catalog__genres-list">
-      {Object.keys(FiltersType).map((tab) => <GenreTab
+      {Object.keys(FiltersTabName).map((tab) => <GenreTab
         tab={tab}
         key={tab}
         onChangeGenres={onChangeGenres}
