29b86f27838d6246870ed294a409edad3e9b0436
Vafin Artem <a.vafin85@gmail.com>
2021-03-10 22:25:57 +0500

    убрал перерендер RatingStart при обновлении компонента AddReviewForm

--- src/components/add-revierw-form/add-review-form.jsx
+++ src/components/add-revierw-form/add-review-form.jsx
@@ -6,6 +6,7 @@ import {postComment} from '../../store/api-actions';
 import {ActionCreator} from '../../store/action';
 
 const RATING_STARS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const DEFAULT_RATING = 10;
 
 const CommentLength = {
   MIN: 50,
@@ -13,34 +14,34 @@ const CommentLength = {
 };
 
 const AddReviewForm = ({filmID, submit, activateForm, isActiveForm}) => {
-  const [userReview, setUserReview] = useState({rating: 10, text: ``});
+  const [userReviewText, setUserReviewText] = useState(``);
+  const [userReviewRating, setUserReviewRating] = useState(DEFAULT_RATING);
 
   useEffect(() => {
-    activateForm(userReview.text.length >= CommentLength.MIN && userReview.text.length <= CommentLength.MAX);
-  }, [userReview]);
+    activateForm(userReviewText.length >= CommentLength.MIN && userReviewText.length <= CommentLength.MAX);
+  }, [userReviewText]);
 
   return (
     <form action="#" className="add-review__form" onSubmit={(evt) => {
       evt.preventDefault();
       activateForm(false);
-      submit(filmID, userReview);
+      submit(filmID, {
+        text: userReviewText,
+        rating: userReviewRating
+      });
     }}>
       <div className="rating">
         <div className="rating__stars">
           {RATING_STARS.map((element) => <RatingStar
             key={element}
             rating={element}
-            userReview={userReview}
-            setUserReview={setUserReview}
+            setUserReviewRating={setUserReviewRating}
           />)}
         </div>
       </div>
       <div className="add-review__text">
         <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text" defaultValue={``} onInput={(evt) => {
-          setUserReview({
-            ...userReview,
-            text: evt.target.value
-          });
+          setUserReviewText(evt.target.value);
         }} />
         <div className="add-review__submit">
           <button className="add-review__btn" type="submit" disabled={isActiveForm ? `` : `disabled`}>Post</button>
