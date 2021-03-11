1ec47992ef9bcef8cd646c49e4796e2a35825661
Vafin Artem <a.vafin85@gmail.com>
2021-03-11 18:43:25 +0500

    Revert "Revert "Revert "убрал коннект в App заменил на хуки"""
    This reverts commit dce70e562b8e8778f7933595da0f83627ca31451.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Switch, Route, Router as BrowserRouter, Redirect} from 'react-router-dom';
-import {useSelector, useDispatch} from 'react-redux';
+import {connect} from 'react-redux';
 import PrivateRoute from '../private-route/private-route';
 import Main from '../main/main';
 import Movie from '../movie/movie';
@@ -11,17 +12,15 @@ import Player from '../player/player';
 import NotFoundPage from '../not-found-page/not-found-page';
 import browserHistory from "../../browser-history";
 import {AuthorizationStatuses, Url} from '../../consts';
+import {MOVIES_PROP, REVIEW_PROP, MOVIES_NOT_REQUIRE_PROP} from '../../utils/validate';
 import {fetchFilm} from '../../store/api-actions';
 import LoadingScreen from '../loading-screen/loading-screen';
+import {getFilms, getFilmsLoadedStatus} from '../../store/films/selectors';
+import {getFilmLoadedStatus, getLoadedFilm} from '../../store/film/selectors';
+import {getReviews} from '../../store/comment/selectors';
+import {getAuthorizationStatus} from '../../store/auth/selectors';
 
-const App = () => {
-  const {films, isFilmsLoaded} = useSelector((state) => state.FILMS);
-  const {loadedFilm, isFilmLoaded} = useSelector((state) => state.FILM);
-  const {authorizationStatus} = useSelector((state) => state.AUTH);
-  const {reviews} = useSelector((state) => state.COMMENT);
-
-  const dispatch = useDispatch();
-
+const App = ({films, reviews, authorizationStatus, loadFilm, loadedFilm, isFilmLoaded, isFilmsLoaded}) => {
   return (
     <BrowserRouter history={browserHistory}>
       <Switch>
@@ -52,7 +51,7 @@ const App = () => {
             />;
           }
           if (!isFilmLoaded) {
-            dispatch(fetchFilm(id));
+            loadFilm(id);
             return <LoadingScreen />;
           }
           return <Movie
@@ -108,4 +107,31 @@ const App = () => {
   );
 };
 
-export default App;
+App.propTypes = {
+  films: PropTypes.arrayOf(PropTypes.shape(MOVIES_PROP).isRequired).isRequired,
+  loadedFilm: PropTypes.shape(MOVIES_NOT_REQUIRE_PROP).isRequired,
+  loadFilm: PropTypes.func.isRequired,
+  isFilmLoaded: PropTypes.bool.isRequired,
+  isFilmsLoaded: PropTypes.bool.isRequired,
+  reviews: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.shape(REVIEW_PROP))).isRequired,
+  authorizationStatus: PropTypes.string.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  films: getFilms(state),
+  loadedFilm: getLoadedFilm(state),
+  reviews: getReviews(state),
+  authorizationStatus: getAuthorizationStatus(state),
+  isFilmLoaded: getFilmLoadedStatus(state),
+  isFilmsLoaded: getFilmsLoadedStatus(state)
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  loadFilm(id) {
+    dispatch(fetchFilm(id));
+  },
+});
+
+
+export {App};
+export default connect(mapStateToProps, mapDispatchToProps)(App);
