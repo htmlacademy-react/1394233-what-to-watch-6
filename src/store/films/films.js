2ff58571576458248598429c2c2822c964dd3c0b
Vafin Artem <a.vafin85@gmail.com>
2021-03-14 20:15:45 +0500

    реализовал добавление и удаление отдельно загруженного фильма в избранное

--- src/store/films/films.js
+++ src/store/films/films.js
@@ -43,39 +43,54 @@ const films = createReducer(initialState, (builder) => {
     state.isFavoriteFilmsLoaded = true;
   });
   builder.addCase(ActionType.ADD_FAVORITE_FILM, (state, action) => {
-    const currentIndexFilm = findFilmIndex(state.films, action.payload.id);
-    state.favoriteFilms = [
-      ...state.favoriteFilms,
-      action.payload
-    ];
-    state.films[currentIndexFilm] = Object.assign(
-        {},
-        state.films[currentIndexFilm],
-        {isFavorite: !state.films[currentIndexFilm].isFavorite}
-    );
-    if (state.promoMovie.id === action.payload.id) {
-      state.promoMovie = Object.assign(
+    if (!state.isFilmsLoaded && state.loadedFilm.id === action.payload.id) {
+      state.loadedFilm = Object.assign(
           {},
-          state.promoMovie,
-          {isFavorite: !state.promoMovie.isFavorite}
+          state.loadedFilm,
+          {isFavorite: !state.loadedFilm.isFavorite}
       );
+    } else {
+      const currentIndexFilm = findFilmIndex(state.films, action.payload.id);
+      state.favoriteFilms = [
+        ...state.favoriteFilms,
+        action.payload
+      ];
+      state.films[currentIndexFilm] = Object.assign(
+          {},
+          state.films[currentIndexFilm],
+          {isFavorite: !state.films[currentIndexFilm].isFavorite}
+      );
+      if (state.promoMovie.id === action.payload.id) {
+        state.promoMovie = Object.assign(
+            {},
+            state.promoMovie,
+            {isFavorite: !state.promoMovie.isFavorite}
+        );
+      }
     }
   });
   builder.addCase(ActionType.REMOVE_FAVORITE_FILM, (state, action) => {
-    const currentIndexFilm = findFilmIndex(state.films, action.payload);
-
-    state.favoriteFilms = state.favoriteFilms.filter((film) => film.id !== action.payload);
-    state.films[currentIndexFilm] = Object.assign(
-        {},
-        state.films[currentIndexFilm],
-        {isFavorite: !state.films[currentIndexFilm].isFavorite}
-    );
-    if (state.promoMovie.id === action.payload) {
-      state.promoMovie = Object.assign(
+    if (!state.isFilmsLoaded && state.loadedFilm.id === action.payload) {
+      state.loadedFilm = Object.assign(
           {},
-          state.promoMovie,
-          {isFavorite: !state.promoMovie.isFavorite}
+          state.loadedFilm,
+          {isFavorite: !state.loadedFilm.isFavorite}
       );
+    } else {
+      const currentIndexFilm = findFilmIndex(state.films, action.payload);
+      state.favoriteFilms = state.favoriteFilms.filter((film) => film.id !== action.payload);
+      state.films[currentIndexFilm] = Object.assign(
+          {},
+          state.films[currentIndexFilm],
+          {isFavorite: !state.films[currentIndexFilm].isFavorite}
+      );
+      if (state.promoMovie.id === action.payload) {
+        state.promoMovie = Object.assign(
+            {},
+            state.promoMovie,
+            {isFavorite: !state.promoMovie.isFavorite}
+        );
+      }
     }
   });
   builder.addCase(ActionType.LOAD_FILM, (state, action) => {
